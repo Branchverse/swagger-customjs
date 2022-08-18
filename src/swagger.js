f3a2a41536c11db23b6b28c9da7557fb4a102f75
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2022-08-18 22:57:20 +0200

    Update README

--- src/swagger.js
+++ src/swagger.js
@@ -1,3 +1,4 @@
+// Callback for the observer
 const callback = (mutationList) => {
   for (const mutation of mutationList) {
     if (mutation.type === 'childList') {
@@ -6,28 +7,35 @@ const callback = (mutationList) => {
   }
 };
 
+// Create observer and Events
 const observer = new MutationObserver(callback);
 const changeEvent = new CustomEvent('change', { bubbles: true });
 const clickEvent = new MouseEvent('click', { bubbles: true });
+
 let authWrapper;
+// Wait for the window being loaded to access the DOM
 window.addEventListener('load', () => {
   authWrapper = document.getElementsByClassName('auth-wrapper')[0];
   observer.observe(authWrapper, { childList: true });
 });
 
+// This is a little hacky example since the fields mostly have no id's to go by, but it works well enough to serve as an example.
 const fillOutForm = () => {
   const client_id =
-    authWrapper.getElementsByClassName('wrapper')[0].childNodes[1].firstChild;
-  if (client_id) {
-    const scopes =
-      authWrapper.getElementsByClassName('scopes')[0].firstChild.childNodes[1];
+    authWrapper.getElementsByClassName('wrapper')[0]?.childNodes[1].firstChild;
 
-    client_id.setAttribute('value', 'From customJs!');
-    client_id.dispatchEvent(changeEvent);
-    scopes.dispatchEvent(clickEvent);
-    // Optionally you can even invoke the auth button immediately if you want to
-    // const authButton =
-    //   authWrapper.getElementsByClassName('auth-btn-wrapper')[0].firstChild;
-    // authButton.dispatchEvent(clickEvent);
-  }
+  // Since fillOutForm is also called on closing the popup, we need to check if the form is open.
+  if (!client_id) return;
+
+  const scopes =
+    authWrapper.getElementsByClassName('scopes')[0].firstChild.childNodes[1];
+  client_id.setAttribute('value', 'From customJs!');
+  client_id.dispatchEvent(changeEvent);
+  scopes.dispatchEvent(clickEvent);
+
+  // Optionally you can even invoke the auth button immediately if you want to
+
+  // const authButton =
+  //   authWrapper.getElementsByClassName('auth-btn-wrapper')[0].firstChild;
+  // authButton.dispatchEvent(clickEvent);
 };
